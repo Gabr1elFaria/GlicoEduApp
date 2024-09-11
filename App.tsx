74f7d8fe9746a5a5996c6cf6745bfc3f16d21a4b
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-11 12:44:52 -0300

    feat: change the return of the app to navigator file

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import 'react-native-gesture-handler';
 import { Text } from 'react-native';
 
+import Navitagor from 'navigation';
+import React from 'react';
+
 export default function App() {
-  return (
-    <>
-      <Text>Hello World</Text>
-    </>
-  );
+  return <Navitagor />;
 }
