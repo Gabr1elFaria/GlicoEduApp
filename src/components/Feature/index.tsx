057437fcba8d2ff1dd34376b10185f10703f9823
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-13 14:37:46 -0300

    feat: add props to manipulate the color and text

--- src/components/Feature/index.tsx
+++ src/components/Feature/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
+import { View, Text, Image, TouchableOpacity, StyleProp, ViewStyle } from 'react-native';
 import { styles } from './style';
 import IconCalculator from 'react-native-vector-icons/FontAwesome5';
 
-export function Feature() {
+export function Feature({
+  backgroundColor,
+  borderColor,
+  text,
+}: {
+  text: string;
+  borderColor: string;
+  backgroundColor: string;
+}) {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Calculadora de Insulina</Text>
+    <View style={[styles.container, { backgroundColor, borderColor }]}>
+      <Text style={styles.text}>{text}</Text>
       <View>
         <IconCalculator name="calculator" style={styles.icon} />
       </View>
