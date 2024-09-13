bc4e04c33305a38d9ed5ebc5ab7ff7c587703f0b
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-13 14:41:39 -0300

    feat: add component as a second header for main features

--- src/screens/InsulinCalc/index.tsx
+++ src/screens/InsulinCalc/index.tsx
@@ -2,14 +2,18 @@ import React from 'react';
 import { Text, View } from 'react-native';
 import { style } from './style';
 import { Header } from '~/components/Header';
+import Footer from '~/components/Footer';
+import { HeaderFeature } from '~/components/HeaderFeature';
 
 export function InsulinCalc() {
   return (
     <>
       <Header />
+      <HeaderFeature backgroundColor="#E069E0" text="CALCULADORA DE INSULINA" />
       <View style={style.container}>
         <Text>tela insulina calculadora</Text>
       </View>
+      <Footer />
     </>
   );
 }
--- src/screens/Quiz/index.tsx
+++ src/screens/Quiz/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { Text } from 'react-native';
+import Footer from '~/components/Footer';
 import { Header } from '~/components/Header';
+import { HeaderFeature } from '~/components/HeaderFeature';
 
 export function Quiz() {
   return (
     <>
       <Header />
+      <HeaderFeature backgroundColor="#39BAAF" text="QUIZ" />
       <Text>tela Quiz</Text>
+      <Footer />
     </>
   );
 }
