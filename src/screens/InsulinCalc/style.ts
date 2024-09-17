8df45b0a59877f5cb83fae88942cd982d8d06527
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-16 21:45:22 -0300

    feat: add styles for Insulin Calculator screen

--- src/screens/InsulinCalc/style.ts
+++ src/screens/InsulinCalc/style.ts
@@ -1,5 +1,53 @@
+import { ViewStyle } from 'react-native';
 import { StyleSheet } from 'react-native';
 
 export const style = StyleSheet.create({
-  container: {},
+  mainContainer: {},
+  container: {
+    width: 'auto',
+    height: 300,
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 40,
+    marginBottom: -60,
+  },
+  text: {
+    fontSize: 22,
+    width: 200,
+    fontWeight: '400',
+    textAlign: 'center',
+  },
+  textInsulin: {
+    width: 280,
+  },
+  textResult: {
+    fontSize: 26,
+    fontWeight: '700',
+    textAlign: 'center',
+  },
+  textInput: {
+    borderWidth: 1,
+    width: 180,
+    fontSize: 22,
+    height: 48,
+    borderRadius: 5,
+    paddingHorizontal: 10,
+    textAlign: 'center',
+  },
+  buttonText: {
+    fontSize: 22,
+    fontWeight: '500',
+    color: '#000',
+  },
+  button: {
+    backgroundColor: '#FFFFFF',
+    borderWidth: 3,
+    borderColor: '#000',
+    borderRadius: 10,
+    width: 120,
+    height: 40,
+    padding: -5,
+  },
+  buttonDisable: {},
 });
