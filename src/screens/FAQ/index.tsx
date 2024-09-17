5305344d71edd4b4e07f3927f5932906a686cfbb
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-16 21:44:28 -0300

    feat: change backGroundColor for headerFeature props

--- src/screens/FAQ/index.tsx
+++ src/screens/FAQ/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Text } from 'react-native';
 import { Header } from '~/components/Header';
+import { HeaderFeature } from '~/components/HeaderFeature';
 
 export function FAQ() {
   return (
     <>
       <Header />
+      <HeaderFeature backgroundColor="#1E1E1E" text="PERGUNTAS FREQUENTES" textColor="#FFFFFF" />
       <Text>tela FAQ</Text>
     </>
   );
