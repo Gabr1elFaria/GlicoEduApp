d843d45f1bc5c94fcc15055c2239b78c66217d1e
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-17 22:33:29 -0300

    feat: create an component to each content inside of the accordion

---
+++ src/components/Accordion/styleContent.ts
@@ -0,0 +1,27 @@
+import { StyleSheet } from 'react-native';
+
+export const style = StyleSheet.create({
+  text: {
+    marginTop: -10,
+    fontSize: 22,
+    fontWeight: '500',
+  },
+  textBox: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 1,
+  },
+  box: {
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  icon: {
+    display: 'flex',
+    width: 40,
+    height: 40,
+  },
+});
