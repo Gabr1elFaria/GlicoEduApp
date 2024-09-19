c28754a8174a72230b37c90eed5200b305c643cc
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-19 14:57:21 -0300

    feat: update import

--- App.tsx
+++ App.tsx
@@ -3,7 +3,7 @@ import 'react-native-gesture-handler';
 import React from 'react';
 
 import Routes from '~/routes';
-import CombinedProvider from '~/providers/RecipeFilterProvider';
+import CombinedProvider from '~/providers/CombinedProvider';
 
 export default function App() {
   return (
