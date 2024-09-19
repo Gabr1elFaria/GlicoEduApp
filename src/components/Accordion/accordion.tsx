ce8cc0c5134f5b33b340c3ba4af11a29602f9ecc
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-19 14:58:35 -0300

    feat: add logic to manipulate the height of the component

--- src/components/Accordion/accordion.tsx
+++ src/components/Accordion/accordion.tsx
@@ -1,24 +1,48 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import { View, Text, TouchableOpacity, Animated } from 'react-native';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { style } from './style';
+import { useGlicRateHeight } from '~/providers/GlicRateHeightProvider';
+import { useMealHeight } from '~/providers/MealHeightProvider';
 
 interface AccordionProps {
   title: string;
   children: React.ReactNode;
+  onToggle: (isOpen: boolean) => void;
 }
 
-export default function Accordion({ title, children }: AccordionProps) {
+export default function Accordion({ title, children, onToggle }: AccordionProps) {
   const [isOpen, setIsOpen] = useState(false);
   const height = useRef(new Animated.Value(0)).current;
+  const { heightGlicRate, setHeightGlicRate } = useGlicRateHeight();
+  const { heightMeal, setHeightMeal } = useMealHeight();
 
   const toggleAccordion = () => {
-    setIsOpen(!isOpen);
+    const newState = !isOpen;
+    setIsOpen(newState);
+    let targetHeight = 0;
+
+    if (isOpen) {
+      if (heightGlicRate && !heightMeal) {
+        targetHeight = 180;
+      } else if (!heightGlicRate && heightMeal) {
+        targetHeight = 260;
+      } else if (heightGlicRate && heightMeal) {
+        targetHeight = 260;
+      } else {
+        targetHeight = 0;
+      }
+    } else {
+      targetHeight = 0;
+    }
+
     Animated.timing(height, {
-      toValue: isOpen ? 0 : 250,
+      toValue: targetHeight,
       duration: 50,
       useNativeDriver: false,
     }).start();
+
+    onToggle(newState);
   };
 
   return (
