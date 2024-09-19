086b7df674d8e48ef6f210089ebf57a00515d80a
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-19 11:08:50 -0300

    feat: wrap routes with provider

--- App.tsx
+++ App.tsx
@@ -3,7 +3,12 @@ import 'react-native-gesture-handler';
 import React from 'react';
 
 import Routes from '~/routes';
+import CombinedProvider from '~/providers/RecipeFilterProvider';
 
 export default function App() {
-  return <Routes />;
+  return (
+    <CombinedProvider>
+      <Routes />
+    </CombinedProvider>
+  );
 }
