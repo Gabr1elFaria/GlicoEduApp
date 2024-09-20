17ea4bfa32dcdffde160b4abde24c69feb1b94da
Gabr1elFaria <gabriel.selu@gmail.com>
2024-09-20 13:23:51 -0300

    feat: update logic to fit with the size of the all components in the screen using context variables

--- src/components/Recipe/index.tsx
+++ src/components/Recipe/index.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect, useState } from 'react';
-import { FlatList, Pressable, ScrollView, Text, View } from 'react-native';
+import React, { useEffect, useRef, useState } from 'react';
+import { Animated, FlatList, Pressable, Text, View } from 'react-native';
 import { style } from './style';
 
 import IconMug from 'react-native-vector-icons/FontAwesome5';
@@ -13,6 +13,8 @@ import { useRecipeGlicFilter } from '~/providers/RecipeGlicFilterProvider';
 import { useRecipeMealFilter } from '~/providers/RecipeMealFilterProvider';
 import { NavigationProp, useNavigation } from '@react-navigation/native';
 import { RootStackParamList } from '~/routes';
+import { useGlicRateHeight } from '~/providers/GlicRateHeightProvider';
+import { useMealHeight } from '~/providers/MealHeightProvider';
 
 interface Recipe {
   id: number;
@@ -32,6 +34,10 @@ export function Recipe() {
   const [recipes, setRecipes] = useState<Recipe[]>([]);
   const { lowGlic, mediumGlic } = useRecipeGlicFilter();
   const { breakfest, lunch, dinner, snack } = useRecipeMealFilter();
+  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  const heightRef = useRef(new Animated.Value(0)).current;
+  const { heightGlicRate } = useGlicRateHeight();
+  const { heightMeal } = useMealHeight();
 
   useEffect(() => {
     const data = require('../../assets/recipes.json');
@@ -59,35 +65,45 @@ export function Recipe() {
     navigation.navigate('ReceitasItSelf', { recipeId: recipe.id });
   };
 
-  const navigation = useNavigation<NavigationProp<RootStackParamList>>();
+  useEffect(() => {
+    const targetHeight = heightGlicRate ? (heightMeal ? 280 : 360) : heightMeal ? 280 : 540;
+    Animated.timing(heightRef, {
+      toValue: targetHeight,
+      duration: 300,
+      useNativeDriver: false,
+    }).start();
+  }, [heightGlicRate, heightMeal]);
 
   return (
     <View>
-      <FlatList
-        data={filteredRecipes}
-        keyExtractor={(item) => item.id.toString()}
-        renderItem={({ item }) => (
-          <Pressable onPress={() => handlePress(item)}>
-            <View style={style.container}>
-              <View style={style.containerText}>
-                <Text style={style.text}>{item.title ? item.title : 'Título não encontrado'}</Text>
-              </View>
-              <View style={style.icon}>
-                {item.category.meal === 'almoco' && <IconSun name="sun" size={30} />}
-                {item.category.meal === 'jantar' && <IconMoon name="moon" size={30} />}
-                {item.category.meal === 'lanche' && <IconApple name="apple" size={30} />}
-                {item.category.meal === 'cafe' && <IconMug name="mug-hot" size={30} />}
+      <Animated.View style={{ height: heightRef }}>
+        <FlatList
+          data={filteredRecipes}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => (
+            <Pressable onPress={() => handlePress(item)}>
+              <View style={style.container}>
+                <View style={style.containerText}>
+                  <Text style={style.text}>
+                    {item.title ? item.title : 'Título não encontrado'}
+                  </Text>
+                </View>
+                <View style={style.icon}>
+                  {item.category.meal === 'almoco' && <IconSun name="sun" size={30} />}
+                  {item.category.meal === 'jantar' && <IconMoon name="moon" size={30} />}
+                  {item.category.meal === 'lanche' && <IconApple name="apple" size={30} />}
+                  {item.category.meal === 'cafe' && <IconMug name="mug-hot" size={30} />}
 
-                {item.category.glicRate === 'baixo' && (
-                  <IconSmile name="slightly-smile" size={30} />
-                )}
-                {item.category.glicRate === 'medio' && <IconSuperSmile name="smiley" size={30} />}
+                  {item.category.glicRate === 'baixo' && (
+                    <IconSmile name="slightly-smile" size={30} />
+                  )}
+                  {item.category.glicRate === 'medio' && <IconSuperSmile name="smiley" size={30} />}
+                </View>
               </View>
-            </View>
-          </Pressable>
-        )}
-        style={style.flatListBox}
-      />
+            </Pressable>
+          )}
+        />
+      </Animated.View>
     </View>
   );
 }
