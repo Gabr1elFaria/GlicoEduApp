7554f037457fdfebbf1f923b0f3ccffea8885f97
Gabr1elFaria <gabriel.selu@gmail.com>
2024-11-02 15:06:04 -0300

    test: create InsulinCalc test file

---
+++ src/screens/InsulinCalc/__tests__/InsulinCalc.test.tsx
@@ -0,0 +1,144 @@
+import React from 'react';
+import '@testing-library/jest-native/extend-expect';
+import { render, fireEvent, waitFor } from '@testing-library/react-native';
+import { InsulinCalc } from '..';
+import { NavigationContainer } from '@react-navigation/native';
+
+describe('InsulinCalc Component', () => {
+  test('renders input, button, and initial text correctly', () => {
+    const { getByPlaceholderText, getByText } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+
+    expect(getByPlaceholderText('Ex: 30')).toBeTruthy();
+    expect(getByText('Insira a quantidade de carboidrato em gramas:')).toBeTruthy();
+    expect(getByText('CALCULAR')).toBeTruthy();
+  });
+
+  test('button is initially disabled', () => {
+    const { getByRole } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const button = getByRole('button', { name: 'CALCULAR' });
+
+    expect(button).toBeDisabled();
+  });
+
+  test('enables button when input has a valid number', async () => {
+    const { getByPlaceholderText, getByRole } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const input = getByPlaceholderText('Ex: 30');
+    const button = getByRole('button', { name: 'CALCULAR' });
+
+    fireEvent.changeText(input, '20');
+
+    await waitFor(() => {
+      expect(button).not.toBeDisabled();
+    });
+  });
+
+  test('disables button when input is cleared', () => {
+    const { getByPlaceholderText, getByRole } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const input = getByPlaceholderText('Ex: 30');
+    const button = getByRole('button', { name: 'CALCULAR' });
+
+    fireEvent.changeText(input, '20');
+    fireEvent.changeText(input, '');
+    expect(button).toBeDisabled();
+  });
+
+  test('calculates correct insulin dose for input 10', async () => {
+    const { getByPlaceholderText, getByRole, findByText } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const input = getByPlaceholderText('Ex: 30');
+
+    fireEvent.changeText(input, '10');
+    fireEvent.press(getByRole('button', { name: 'CALCULAR' }));
+
+    expect(
+      await findByText('A dose necessária de insulina para 10 gramas de carboidrato é:')
+    ).toBeTruthy();
+    expect(await findByText('Uma ( 1 ) Unidade')).toBeTruthy();
+  });
+
+  test('calculates correct insulin dose for input 50', async () => {
+    const { getByPlaceholderText, getByRole, findByText } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const input = getByPlaceholderText('Ex: 30');
+
+    fireEvent.changeText(input, '50');
+    fireEvent.press(getByRole('button', { name: 'CALCULAR' }));
+
+    expect(
+      await findByText('A dose necessária de insulina para 50 gramas de carboidrato é:')
+    ).toBeTruthy();
+    expect(await findByText('Quatro ( 4 ) Unidades')).toBeTruthy();
+  });
+
+  test('calculates "Não foi possível calcular" for values above 75', async () => {
+    const { getByPlaceholderText, getByRole, findByText } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const input = getByPlaceholderText('Ex: 30');
+
+    fireEvent.changeText(input, '80');
+    fireEvent.press(getByRole('button', { name: 'CALCULAR' }));
+
+    expect(await findByText('Não foi possível calcular')).toBeTruthy();
+  });
+
+  test('resets insulin result display when input is cleared', async () => {
+    const { getByPlaceholderText, getByRole, getByText, queryByText } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const input = getByPlaceholderText('Ex: 30');
+
+    fireEvent.changeText(input, '15');
+    fireEvent.press(getByRole('button', { name: 'CALCULAR' }));
+
+    await waitFor(() =>
+      expect(
+        getByText('A dose necessária de insulina para 15 gramas de carboidrato é:')
+      ).toBeTruthy()
+    );
+
+    fireEvent.changeText(input, '');
+    await waitFor(() => expect(queryByText('A dose necessária de insulina para')).toBeNull());
+  });
+
+  test('calls handleTouchOutside when pressing outside the input', () => {
+    const { getByPlaceholderText, getByRole, getByText, getByDisplayValue } = render(
+      <NavigationContainer>
+        <InsulinCalc />
+      </NavigationContainer>
+    );
+    const input = getByPlaceholderText('Ex: 30');
+
+    fireEvent.changeText(input, '20');
+    fireEvent.press(getByRole('button', { name: 'CALCULAR' }));
+    fireEvent.press(getByText('Insira a quantidade de carboidrato em gramas:'));
+
+    expect(getByDisplayValue('20')).toBeTruthy();
+  });
+});
