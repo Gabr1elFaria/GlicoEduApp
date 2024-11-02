26e3a9387612c4fd5fc7338cb5b76fe13398a187
Gabr1elFaria <gabriel.selu@gmail.com>
2024-11-02 15:05:47 -0300

    build: update and add new dependencies

---
+++ jest.config.js
@@ -0,0 +1,19 @@
+const { pathsToModuleNameMapper } = require('ts-jest');
+const { compilerOptions } = require('./tsconfig');
+
+module.exports = {
+    preset: 'react-native',
+    setupFilesAfterEnv: [
+        '@testing-library/jest-native/extend-expect'
+    ],
+    transformIgnorePatterns: [],
+    moduleFileExtensions: [
+        'ts',
+        'tsx',
+        'js',
+        'jsx',
+        'json',
+        'node'
+    ],
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths || {}, { prefix: '<rootDir>/' }),
+};
